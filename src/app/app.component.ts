7bf27f9198050b16cb5a5bd6dc982be8e8eeda01
Finn Watermann <finn@w-mi.de>
2023-12-31 15:51:57 +0100

    Countdown v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {CountdownComponent} from "./countdown/countdown.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CountdownComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Countdown';
+
+  private interval: any;
+  private version: number = 0;
+
+  fetchVersion(): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+      fetch('/version.txt').then(response => {
+        if (response.ok) {
+          response.text().then(text => {
+            resolve(parseInt(text));
+          });
+        } else {
+          reject(response);
+        }
+      });
+    });
+  }
+
+  ngOnInit() {
+    this.fetchVersion().then(version => {
+      this.version = version;
+      this.interval = setInterval(() => {
+        this.fetchVersion().then(version => {
+          if (version > this.version) {
+            window.location.reload();
+          }
+        });
+      }, 10000);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
 }
---
+++ src/app/countdown/countdown.component.ts
@@ -0,0 +1,97 @@
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {NgIf} from "@angular/common";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-countdown',
+  standalone: true,
+  imports: [
+    NgIf
+  ],
+  templateUrl: './countdown.component.html',
+  styleUrl: './countdown.component.scss'
+})
+export class CountdownComponent implements OnInit {
+
+  protected days: number = 0;
+  protected hours: number = 0;
+  protected minutes: number = 0;
+  protected seconds: number = 0;
+  protected milliseconds: number = 0;
+  protected text: string = "";
+
+  private interval?: any;
+
+  private TARGET: number = new Date(new Date().getFullYear() + 1, 0, 1, 0, 0, 0, 0).getTime();
+  private TEXT = "Frohes neues Jahr!";
+
+  constructor(private element: ElementRef, private route: ActivatedRoute) {
+    this.element.nativeElement.style.fontSize = "14vW";
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      Object.keys(params).forEach(key => {
+          switch(key) {
+            case 't':
+              this.TARGET = parseInt(params[key]);
+              break;
+            case 'tr':
+              this.TARGET = Date.now() + parseInt(params[key]);
+              break;
+            case 'text':
+              this.TEXT = params[key];
+              break;
+          }
+      });
+    });
+  }
+
+  ngAfterViewInit() {
+    this.interval = setInterval(() => {
+      this.updateNumbers(this.TARGET - Date.now());
+    }, 50);
+  }
+
+  ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  private updateNumbers(msLeft: number) {
+    if(msLeft < 0) {
+      this.days = 0;
+      this.hours = 0;
+      this.minutes = 0;
+      this.seconds = 0;
+      this.milliseconds = 0;
+      this.text = this.TEXT;
+      return;
+    }
+    let calc = msLeft;
+    this.days = Math.floor(calc / (1000 * 60 * 60 * 24));
+    calc -= this.days * (1000 * 60 * 60 * 24);
+    this.hours = Math.floor(calc / (1000 * 60 * 60));
+    calc -= this.hours * (1000 * 60 * 60);
+    this.minutes = Math.floor(calc / (1000 * 60));
+    calc -= this.minutes * (1000 * 60);
+    this.seconds = Math.floor(calc / 1000);
+    calc -= this.seconds * 1000;
+    this.milliseconds = Math.floor(calc / 100);
+
+    if(this.days > 0) {
+      this.element.nativeElement.style.fontSize = "14vW";
+    } else if(this.hours > 0) {
+      this.element.nativeElement.style.fontSize = "18vW";
+    } else if(this.minutes > 0) {
+      this.element.nativeElement.style.fontSize = "26vW";
+    } else if(this.seconds > 0) {
+      this.element.nativeElement.style.fontSize = "40vW";
+    } else if(this.milliseconds == 0) {
+      this.element.nativeElement.style.fontSize = "14vW";
+    }
+
+  }
+
+}
